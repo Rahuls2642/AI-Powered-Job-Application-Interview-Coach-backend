95cfccf19148931472053382cb3eabfcc65694e8
Rahul singh <rahuls6408@gmail.com>
2025-12-30 14:21:13 +0530

    Added cors origin

--- app.ts
+++ app.ts
@@ -13,7 +13,10 @@ import reportsRoutes from './src/routes/report.routes';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
