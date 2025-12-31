1db84b6d3b86668f8b13f3355aba44983d171032
Rahuls2642 <135409027+Rahuls2642@users.noreply.github.com>
2025-12-31 14:02:40 +0530

    Update CORS origin to production URL

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,6 @@ import reportsRoutes from './routes/report.routes.js';
 const app = express();
 
 app.use(cors({
-    origin:"http://localhost:5173",
     origin:"https://interview-master-frontend.onrender.com",
     credentials: true,
 }));
